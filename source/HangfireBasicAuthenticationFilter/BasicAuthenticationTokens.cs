df456ff41ed6df3f76dd3b2252f24f5b3a7dc7f6
T-rav <t@bizvoip,co.za>
2019-01-10 08:55:16 -0700

    init commit

---
+++ source/HangfireBasicAuthenticationFilter/BasicAuthenticationTokens.cs
@@ -0,0 +1,35 @@
+﻿namespace HangfireBasicAuthenticationFilter
+{
+    public class BasicAuthenticationTokens
+    {
+        private readonly string[] _tokens;
+
+        public string Username => _tokens[0];
+        public string Password => _tokens[1];
+
+        public BasicAuthenticationTokens(string[] tokens)
+        {
+            _tokens = tokens;
+        }
+
+        public bool Are_Invalid()
+        {
+            return Contains_Two_Tokens() && Valid_Token_Value(Username) && Valid_Token_Value(Password);
+        }
+
+        public bool Credentials_Match(string user, string pass)
+        {
+            return Username.Equals(user) && Password.Equals(pass);
+        }
+
+        private bool Valid_Token_Value(string token)
+        {
+            return !string.IsNullOrWhiteSpace(token);
+        }
+
+        private bool Contains_Two_Tokens()
+        {
+            return _tokens.Length == 2;
+        }
+    }
+}
---
+++ source/HangfireBasicAuthenticationFilter/HangfireCustomBasicAuthenticationFilter.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Net.Http.Headers;
+using Hangfire.Dashboard;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Primitives;
+
+namespace HangfireBasicAuthenticationFilter
+{
+    public class HangfireCustomBasicAuthenticationFilter : IDashboardAuthorizationFilter
+    {
+        public string User { get; set; }
+        public string Pass { get; set; }
+
+        private const string _AuthenticationScheme = "Basic";
+
+        // https://gist.github.com/ndc/a1cc8e2515e5e0d941a884fc6a6267f5
+        public bool Authorize(DashboardContext context)
+        {
+            var httpContext = context.GetHttpContext();
+            var header = httpContext.Request.Headers["Authorization"];
+
+            if (Missing_Authorization_Header(header))
+            {
+                SetChallengeResponse(httpContext);
+                return false;
+            }
+
+            var authValues = AuthenticationHeaderValue.Parse(header);
+
+            if (Not_Basic_Authentication(authValues))
+            {
+                SetChallengeResponse(httpContext);
+                return false;
+            }
+
+            var tokens = Extract_Authentication_Tokens(authValues);
+
+            if (tokens.Are_Invalid())
+            {
+                SetChallengeResponse(httpContext);
+                return false;
+            }
+
+            if (tokens.Credentials_Match(User, Pass))
+            {
+                return true;
+            }
+
+            SetChallengeResponse(httpContext);
+            return false;
+        }
+
+        private static bool Missing_Authorization_Header(StringValues header)
+        {
+            return string.IsNullOrWhiteSpace(header);
+        }
+
+        private static BasicAuthenticationTokens Extract_Authentication_Tokens(AuthenticationHeaderValue authValues)
+        {
+            var parameter = System.Text.Encoding.UTF8.GetString(Convert.FromBase64String(authValues.Parameter));
+            var parts = parameter.Split(':');
+            return new BasicAuthenticationTokens(parts);
+        }
+
+        private static bool Not_Basic_Authentication(AuthenticationHeaderValue authValues)
+        {
+            return !_AuthenticationScheme.Equals(authValues.Scheme, StringComparison.InvariantCultureIgnoreCase);
+        }
+
+        private void SetChallengeResponse(HttpContext httpContext)
+        {
+            httpContext.Response.StatusCode = 401;
+            httpContext.Response.Headers.Append("WWW-Authenticate", "Basic realm=\"Hangfire Dashboard\"");
+            httpContext.Response.WriteAsync("Authentication is required.");
+        }
+    }
+}
